e269b6dd03dff0ca26d960e3709cba75c5afbf73
LuiTheImpeccable <Luis.Rodriguez.15@wheels-student.org>
2017-10-03 16:41:59 -0400

    GD205 Week 4 edit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    
+    //Component that adds the opportunity to control Player's Position using Physics Engine
+    private Rigidbody rb;
+	public AudioClip impact;
+	AudioSource audioSource;
+
+    //
+    public float speed;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        //allows Player Movement through the 2 different axises, Up & Down, Left & Right
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+	}
+}
