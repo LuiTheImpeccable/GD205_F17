4d8ced3fec2cf3598c7a4b85c15313235c8822ff
LuiTheImpeccable <Luis.Rodriguez.15@wheels-student.org>
2017-10-16 11:48:25 -0400

    GD205 - Carnival Game Attempt

---
+++ GD205 - Carnival game/Assets/Scripts/ShootProjectile.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class ShootProjectile : MonoBehaviour {
+	public float damage = 5f;
+	public float range = 100f; 
+	public Camera fpsCam;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown("Fire1"));
+		{
+			Fire ();
+
+		}
+
+	}
+
+	void Fire()
+
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range));
+		{
+
+			Debug.Log (hit.transform.name);
+
+		}
+	}
+}
