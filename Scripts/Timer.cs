c85af95bf145f42e7f26c1beb7b9be8d6eebfd5b
LuiTheImpeccable <Luis.Rodriguez.15@wheels-student.org>
2017-10-30 21:51:48 -0400

    Add files via upload

---
+++ Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public Text GameTimerText;
+	float gameTimer = 0f; 
+
+	// Update is called once per frame
+	void Update () {
+
+		gameTimer += Time.deltaTime;
+
+		int seconds = (int)(gameTimer % 60);
+
+		int minutes = (int)(gameTimer / 60) % 60;
+
+		int hours = (int)(gameTimer / 3600) % 24;
+
+		string timerString = string.Format ("(0.0)(1.00):(2:00)", hours, minutes, seconds);
+
+		GameTimerText.text = timerString;
+
+	}
+
+}
